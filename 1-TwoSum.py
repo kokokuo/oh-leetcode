aee54a26a5f7902d572ab2fd02b817e4265f95f2
kokokuo <v6610688@gmail.com>
2019-07-31 16:11:32 +0800

    1. Add gitignore and Pipfile 2. Add #1 - TwoSum

---
+++ 1-TwoSum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        拿 target 減去 val 的 remaining 放入字典中，下次尋訪便可以先直接找字典有無匹配。
+        若無匹配，則再次拿輸入的 target 減去當下 val 取得 remaining 再次放入字典中，
+        作為新的可能結果，並持續尋找。
+        """
+        found = {}
+        for i, val in enumerate(nums):
+            if val in found.keys():
+                indexes = [found[val], i]
+                return indexes 
+            else:
+                remaining = target - val
+                found[remaining] = i
+        return []
+            
